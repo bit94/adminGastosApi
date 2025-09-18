64b8b105453013a0e63e30c75d884f358c8cb40e
gbautista <gerardobautista0894@gmail.com>
2025-09-18 11:32:42 -0600

    [upd] descripcion

--- src/main/java/com/gbb/adminGastosApi/mapper/TipoPeriodoMapper.java
+++ src/main/java/com/gbb/adminGastosApi/mapper/TipoPeriodoMapper.java
@@ -1,6 +1,7 @@
 package com.gbb.adminGastosApi.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.gbb.adminGastosApi.dto.TipoPeriodoDTO;
@@ -10,7 +11,9 @@ import com.gbb.adminGastosApi.model.TipoPeriodo;
 public interface TipoPeriodoMapper {
 	TipoPeriodoMapper INSTANCE = Mappers.getMapper(TipoPeriodoMapper.class);
 
+	@Mapping(source = "descripcion", target = "Descripcion")
 	TipoPeriodo toEntity(TipoPeriodoDTO dto);
 
+	@Mapping(source = "Descripcion", target = "descripcion")
 	TipoPeriodoDTO toDTO(TipoPeriodo entity);
 }
